67cbbdc65f129005192fbe3dcf85e83d13a5a42f
Deepak Gupta <ddeepakgpt1995@gmail.com>
2023-11-20 16:10:41 +0530

    login page ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layout/header/header.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { DashboardSidemenuComponent } from './layout/dashboard-sidemenu/dashboard-sidemenu.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    DashboardSidemenuComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layout/dashboard-sidemenu/dashboard-sidemenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-sidemenu',
+  templateUrl: './dashboard-sidemenu.component.html',
+  styleUrls: ['./dashboard-sidemenu.component.css']
+})
+export class DashboardSidemenuComponent {
+
+}
